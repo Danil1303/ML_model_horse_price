7ec153ed62acf9199d07361b698c12f037079e08
review_bomber_specdor <91407532+Danil1303@users.noreply.github.com>
2023-10-16 15:26:54 +0300

    Доработка парсера под нужды QThread.

--- get_data.py
+++ get_data.py
@@ -12,21 +12,28 @@ from PyQt5.QtWidgets import QPlainTextEdit
 from requests.adapters import HTTPAdapter, Retry
 
 
-def parse_initialize(log_plain_text_edit: QPlainTextEdit):
-    parse_start = time()
+def parse_initialize():
+    # Задание параметров для парсера
+
     session = requests.Session()
     retry = Retry(connect=5, backoff_factor=1)
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('http://', adapter)
     session.mount('https://', adapter)
-
     url = 'https://www.equestrian.ru'
     category = '/market/horses/'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                              'Chrome/110.0.0.0 YaBrowser/23.3.3.719 Yowser/2.5 Safari/537.36'}
 
-    dollar_url = 'https://www.google.ru/search?newwindow=1&sca_esv=556757575&sxsrf=AB5stBiwKyw7D6S3QgvMZkY3UZxhk2So9A:1692019453036&q=Курс+доллара+к+рублю+на+сегодня&uds=H4sIAAAAAAAA_-Oq5BK-MPfCBoWLjRe2Xth8Yd-FLRf2XuwXUr2w62LzxYaLjQpAgX0XdgPhhosNQHUXdikAhZsvbLyw-2KfAVOR5YVZRChUuLAXww4AoNTqbnsAAAA&sa=X&ved=2ahUKEwiR2M3Tn9yAAxUWIBAIHfZKDXIQxKsJegQICxAB&ictx=0&biw=867&bih=724&dpr=1.25'
-    euro_url = 'https://www.google.ru/search?newwindow=1&sca_esv=556757575&sxsrf=AB5stBjzP_7eUM5V7wzNNpWaOMAl82TB6g:1692019571030&q=Курс+евро+к+рублю+на+сегодня&uds=H4sIAAAAAAAA_-PK5RK-MPfCBoWLjRe2Xth8Yd-FLRf2XuwXkr-w62LzxYaLjQpA4U0XGy7sU7iwSwEo0Hxh44XdF_sMmIqML8zCq0Thwl4McwF6BQTgbwAAAA&sa=X&ved=2ahUKEwiYx--LoNyAAxWNIhAIHTCBCssQxKsJegQICRAB&ictx=0&biw=1488&bih=724&dpr=1.25'
+    dollar_url = ('https://www.google.ru/search?newwindow=1&sca_esv=556757575&sxsrf=AB5stBiwKyw7D6S3QgvMZkY3UZxhk2So9A:'
+                  '1692019453036&q=Курс+доллара+к+рублю+на+сегодня&uds=H4sIAAAAAAAA_-Oq5BK-MPfCBoWLjRe2Xth8Yd-FLRf2XuwX'
+                  'Ur2w62LzxYaLjQpAgX0XdgPhhosNQHUXdikAhZsvbLyw-2KfAVOR5YVZRChUuLAXww4AoNTqbnsAAAA&sa=X&ved=2ahUKEwiR2M'
+                  '3Tn9yAAxUWIBAIHfZKDXIQxKsJegQICxAB&ictx=0&biw=867&bih=724&dpr=1.25')
+    euro_url = ('https://www.google.ru/search?newwindow=1&sca_esv=556757575&sxsrf=AB5stBjzP_7eUM5V7wzNNpWaOMAl82TB6g:16'
+                '92019571030&q=Курс+евро+к+рублю+на+сегодня&uds=H4sIAAAAAAAA_-PK5RK-MPfCBoWLjRe2Xth8Yd-FLRf2XuwXkr-w62L'
+                'zxYaLjQpA4U0XGy7sU7iwSwEo0Hxh44XdF_sMmIqML8zCq0Thwl4McwF6BQTgbwAAAA&sa=X&ved=2ahUKEwiYx--LoNyAAxWNIhAI'
+                'HTCBCssQxKsJegQICRAB&ictx=0&biw=1488&bih=724&dpr=1.25')
+
     response = requests.get(dollar_url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
     soup_dollar_course = soup.findAll("span", {"class": "DFlfde SwHCTb", "data-precision": 2})
@@ -38,74 +45,13 @@ def parse_initialize(log_plain_text_edit: QPlainTextEdit):
     euro_course = float(soup_euro_course[0].text.replace(',', '.'))
 
     data = []
-    # columns = ['Name', 'Price', 'Age', 'Sex', 'Color', 'Height', 'Sizes', 'Breed', 'Advertisement data',
-    #            'Region', 'Location', 'Horse club', 'Contacts']
-    log_plain_text_edit.insertPlainText(f'Старт парсинга.\n')
+
     return session, url, category, dollar_course, euro_course, data
-    # while flag:
-    #     start = time()
-    #     page = f'?p={p}'
-    #     response = session.get(url + category + (page if p else ''))
-    #     p += 1
-    #     soup = BeautifulSoup(response.content, 'html.parser')
-    #     contents = soup.findAll('div', class_='offer')
-    #
-    #     if not contents:
-    #         break
-    #
-    #     links = []
-    #     for item in contents:
-    #         try:
-    #             name = next(iter(item.find('a', href=True))).strip()
-    #             price = next(iter(item.find('div', class_='price'))).strip()
-    #             links.append(item.find('a', href=True)['href'])
-    #         except StopIteration:
-    #             log_plain_text_edit.insertPlainText(
-    #                 f'Пустое объявление по адресу {url + item.find("a", href=True)["href"]}.\n')
-    #         else:
-    #             try:
-    #                 if price == 'цена по запросу':
-    #                     price = None
-    #                 elif '$' in price:
-    #                     price = int(int(price.split(' ')[0].replace(' ', '')) * dollar_course)
-    #                 elif '€' in price:
-    #                     price = int(int(price.split(' ')[0].replace(' ', '')) * euro_course)
-    #                 else:
-    #                     price = int(price.split(' руб.')[0].replace(' ', ''))
-    #             except:
-    #                 price = None
-    #             data.append([name, price])
-    #
-    #     for link in links:
-    #         response = session.get(url + link)
-    #         soup = BeautifulSoup(response.content, 'html.parser')
-    #         try:
-    #             table = soup.find('table', class_='market_descr')
-    #             rows = table.findAll('tr')
-    #         except AttributeError:
-    #             log_plain_text_edit.insertPlainText(f'Не считалась таблица по адресу {url + link}.\n')
-    #         else:
-    #             for row in rows:
-    #                 cols = row.find_all('td')
-    #                 cols = [element.text.strip() for element in cols]
-    #                 data[-1].append(next(element.capitalize() if element else None for element in cols[1:]))
-    #             # i += 1
-    #     log_plain_text_edit.insertPlainText(f'Обработана страница {p} за {round(time() - start, 2)} сек.\n')
-    #
-    # df = pd.DataFrame(data, columns=columns)
-    #
-    # if not os.path.exists('raw_data'):
-    #     os.makedirs('raw_data')
-    # df.to_csv(f'raw_data/equestrian_{len(glob.glob("raw_data/equestrian*")) + 1}.csv', encoding='utf-8-sig',
-    #           index=False)
-    # parse_time = time() - parse_start
-    # minutes = int(parse_time / 60)
-    # seconds = round(parse_time - minutes * 60, 2)
-    # log_plain_text_edit.insertPlainText(
-    #     f'Парсинг занял {str(minutes) + " мин. " if minutes else ""}{seconds} сек., получено {len(data)} записей.\n')
 
 
-def parse(parse_parameters: tuple, p: int, log_plain_text_edit: QPlainTextEdit) -> None:
+def parse(parse_parameters: tuple, p: int, log_plain_text_edit: QPlainTextEdit) -> bool:
+    # Парсер, получающий информацию со всех записей на одной странице
+
     session = parse_parameters[0]
     url = parse_parameters[1]
     category = parse_parameters[2]
@@ -115,12 +61,10 @@ def parse(parse_parameters: tuple, p: int, log_plain_text_edit: QPlainTextEdit) 
     start = time()
     page = f'?p={p}'
     response = session.get(url + category + (page if p else ''))
-    # p += 1
     soup = BeautifulSoup(response.content, 'html.parser')
     contents = soup.findAll('div', class_='offer')
 
     if contents:
-        links = []
         for item in contents:
             try:
                 name = next(iter(item.find('a', href=True))).strip()
@@ -155,11 +99,15 @@ def parse(parse_parameters: tuple, p: int, log_plain_text_edit: QPlainTextEdit) 
                         cols = row.find_all('td')
                         cols = [element.text.strip() for element in cols]
                         data[-1].append(next(element.capitalize() if element else None for element in cols[1:]))
-                    # i += 1
-    log_plain_text_edit.insertPlainText(f'Обработана страница {p + 1} за {round(time() - start, 2)} сек.\n')
+        log_plain_text_edit.insertPlainText(f'Обработана страница {p + 1} за {round(time() - start, 2)} сек.\n')
+    # Флаг, определяющий будет ли продолжаться парсинг
+        return True
+    else:
+        return False
 
 
-def create_df(data: list) -> pd.DataFrame:
+def create_df(data: list):
+    # Функция, сохраняющая полученные с сайта записи в файл
     columns = ['Name', 'Price', 'Age', 'Sex', 'Color', 'Height', 'Sizes', 'Breed', 'Advertisement data',
                'Region', 'Location', 'Horse club', 'Contacts']
     df = pd.DataFrame(data, columns=columns)
@@ -167,20 +115,15 @@ def create_df(data: list) -> pd.DataFrame:
     if not os.path.exists('raw_data'):
         os.makedirs('raw_data')
     current_date = str(date.today()).replace('-', '.')
-    current_date=current_date[5:]+ '.'+ current_date[:4]
+    current_date = current_date[8:] + current_date[4:7] + '.' + current_date[:4]
 
     if not os.path.exists(f'raw_data/{current_date}'):
         os.makedirs(f'raw_data/{current_date}')
-        file_number = len(glob.glob('clean_data/*')) + 1
+        file_number = len(glob.glob('raw_data/*'))
     else:
-        file_number = len(glob.glob('clean_data/*'))
+        file_number = len(glob.glob('raw_data/*'))
     df.to_csv(f'raw_data/{current_date}/equestrian_{file_number}.csv',
               encoding='utf-8-sig', index=False)
-    # parse_time = time() - parse_start
-    # minutes = int(parse_time / 60)
-    # seconds = round(parse_time - minutes * 60, 2)
-    # log_plain_text_edit.insertPlainText(
-    #     f'Парсинг занял {str(minutes) + " мин. " if minutes else ""}{seconds} сек., получено {len(data)} записей.\n')
 
 
 def get_breeds() -> list[str]:
@@ -273,10 +216,9 @@ def age_clear(age: str) -> Union[int, None]:
 
 
 def clear_data(log_plain_text_edit: QPlainTextEdit) -> None:
-    # raw_len = len(df.index)
     current_date = str(date.today()).replace('-', '.')
-    current_date=current_date[5:]+ '.'+ current_date[:4]
-    df=pd.read_csv(f'raw_data/{current_date}/equestrian_{len(glob.glob("raw_data/**"))}.csv')
+    current_date = current_date[8:] + current_date[4:7] + '.' + current_date[:4]
+    df = pd.read_csv(f'raw_data/{current_date}/equestrian_{len(glob.glob("raw_data/**"))}.csv')
     df.dropna(subset=['Price'], inplace=True)
     df.dropna(subset=['Color'], inplace=True)
     df.dropna(subset=['Height'], inplace=True)
@@ -305,15 +247,17 @@ def clear_data(log_plain_text_edit: QPlainTextEdit) -> None:
     df.dropna(subset=['Height'], inplace=True)
     df.dropna(subset=['Color'], inplace=True)
 
+    # Сохранение обработанных данных
     if not os.path.exists('clean_data'):
         os.makedirs('clean_data')
 
     if not os.path.exists(f'clean_data/{current_date}'):
         os.makedirs(f'clean_data/{current_date}')
-        file_number=len(glob.glob('clean_data/*')) + 1
+        file_number = len(glob.glob('clean_data/*'))
     else:
         file_number = len(glob.glob('clean_data/*'))
+
     df.to_csv(f'clean_data/{current_date}/equestrian_{file_number}.csv', encoding='utf-8-sig',
               index=False)
 
-    log_plain_text_edit.insertPlainText(f'После очистки осталось {len(df.index)} записей.')
+    log_plain_text_edit.insertPlainText(f'После очистки осталось {len(df.index)} записей.\n')
