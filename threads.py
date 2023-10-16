c4e5f7482ff8977f5506e9b72cd1e013df4eae56
review_bomber_specdor <91407532+Danil1303@users.noreply.github.com>
2023-10-16 15:31:34 +0300

    Доработка цикла парсера в QThread.

--- threads.py
+++ threads.py
@@ -1,10 +1,6 @@
-import sys
-import socket
 import get_data
-from time import localtime, strftime, sleep
-from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QLineEdit, QPlainTextEdit, QPushButton, \
-    QMessageBox
+from time import time
+from PyQt5.QtCore import QThread
 
 
 class ParseThread(QThread):
@@ -17,14 +13,24 @@ class ParseThread(QThread):
         self.run_parse_thread()
 
     def run_parse_thread(self):
-        # get_data.clear_data(self.log_plain_text_edit)
-        parse_parameters = get_data.parse_initialize(self.log_plain_text_edit)
+        self.log_plain_text_edit.insertPlainText(f'Старт парсинга.\n')
+        parse_start = time()
+        parse_parameters = get_data.parse_initialize()
         data = parse_parameters[5]
         p = 0
         while self.flag:
-            get_data.parse(parse_parameters, p , self.log_plain_text_edit)
-            p+=1
+            parse_status = get_data.parse(parse_parameters, p, self.log_plain_text_edit)
+            if not parse_status:
+                break
+            p += 1
         get_data.create_df(data)
+        parse_time = time() - parse_start
+        minutes = int(parse_time / 60)
+        seconds = round(parse_time - minutes * 60, 2)
+        self.log_plain_text_edit.insertPlainText(
+            f'Парсинг занял {str(minutes) + " мин. " if minutes else ""}{seconds} сек., получено '
+            f'{len(data)} записей.\n')
         get_data.clear_data(self.log_plain_text_edit)
+
     def stop(self):
         self.flag = False
